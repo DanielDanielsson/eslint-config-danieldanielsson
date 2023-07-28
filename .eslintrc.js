3b5abb2cd9a29d4521045e3fd984dac594f4695e
DanielDanielsson <danielssondaniel94@gmail.com>
2023-07-28 16:43:23 +0200

    fix: rm node modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,5 @@
 module.exports = {
-  extends: [
-    "airbnb-typescript",
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended",
-    "prettier/react",
-  ],
+  extends: ["airbnb-typescript"],
   plugins: ["react-hooks"],
   rules: {},
 };
