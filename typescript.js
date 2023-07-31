f361e1ca0909fae7bd216287c108853b751019b1
DanielDanielsson <danielssondaniel94@gmail.com>
2023-07-31 12:50:30 +0200

    feat: separate js and ts rules

---
+++ typescript.js
@@ -0,0 +1,37 @@
+module.exports = {
+    globals: {
+      React: true,
+      JSX: true,
+    },
+    extends: [
+      'plugin:@typescript-eslint/recommended', // Uses rules from `@typescript-eslint/eslint-plugin`,
+      'airbnb-typescript',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      // Layer in all the JS Rules
+      './.eslintrc.js',
+    ],
+    // then add some extra good stuff for Typescript
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    // Then we add our own custom typescript rules
+    rules: {
+    //   '@typescript-eslint/no-explicit-any': 'off',
+      'no-unused-vars': 0,
+      '@typescript-eslint/no-unused-vars': [1, { ignoreRestSiblings: true }],
+      'no-redeclare': 'off',
+      '@typescript-eslint/no-redeclare': [
+        'warn',
+        {
+          ignoreDeclarationMerge: true,
+        },
+      ],
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      // This is covered by the typescript compiler, so we don't need these rules
+      'no-undef': 'off',
+      'no-shadow': 'off', 
+    },
+    parserOptions: {
+      project: './tsconfig.json',
+    },
+  };
