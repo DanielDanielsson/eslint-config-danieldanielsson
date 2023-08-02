30559d9a6a24fdd083dcc6be59029c72df30d8f4
DanielDanielsson <danielssondaniel94@gmail.com>
2023-08-02 17:13:30 +0200

    feat: add some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   extends: ['airbnb', 'prettier'],
   parser: '@typescript-eslint/parser',
+  ignorePatterns: [
+    '.eslintrc.js',
+    'postcss.config.js',
+    'next.config.js',
+    'tailwind.config.js',
+  ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
@@ -22,6 +28,21 @@ module.exports = {
         endOfLine: 'auto',
         tabWidth: 2,
       },
+    ],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+    'import/extensions': 0,
+    "import/no-default-export": 2,
+    "react/function-component-definition": [
+      "error",
+      {
+        "namedComponents": "arrow-function",
+        "unnamedComponents": "arrow-function"
+      }
     ],
   },
   plugins: ['prettier'],
