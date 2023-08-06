601f04a4a1d86f87da3062e641ed49baa7fc91d7
DanielDanielsson <danielssondaniel94@gmail.com>
2023-08-06 08:08:25 +0200

    feat: add eslint types

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
         singleQuote: true,
         endOfLine: 'auto',
         tabWidth: 2,
+        quoteProps: "consistent"
       },
     ],
     'react/jsx-filename-extension': [
