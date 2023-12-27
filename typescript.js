635410aae61db3f0564de9058a4d160992c2c104
DanielDanielsson <danielssondaniel94@gmail.com>
2023-12-27 18:14:08 +0100

    chore: clean up

--- typescript.js
+++ typescript.js
@@ -4,10 +4,9 @@ module.exports = {
     JSX: true,
   },
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses rules from `@typescript-eslint/eslint-plugin`,
+    'plugin:@typescript-eslint/recommended',
     'airbnb-typescript',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    // Layer in all the JS Rules
     './.eslintrc.js',
   ],
   // then add some extra good stuff for Typescript
@@ -20,15 +19,10 @@ module.exports = {
   rules: {
     'react/require-default-props': 0,
     'import/prefer-default-export': 'off',
-
-    // This is covered by the typescript compiler, so we don't need these rules
     'no-undef': 'off',
-    'no-shadow': 'off',
     'typescript-sort/sort-interface': 'error',
     'typescript-sort/sort-type': 'error',
     'typescript-sort/sort-enum': 'error',
     'typescript-sort/sort-arrowfunc-object-params': 'error',
   },
-  // This rule will enforce importing TS types via a type import.
-  // 'consistent-type-imports': 'error',
 };
